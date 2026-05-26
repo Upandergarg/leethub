c27f573fa51ac0b9d6ccfb66e75e6d1884df525c
Upander Garg <upandergarg@gmail.com>
2026-05-26 19:20:03 +0530

    Time: 7 ms (50.87%), Space: 67.9 MB (9.16%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class w{
+        int min;
+        int max;
+        int sum;
+        w(int min,int max,int sum){
+this.min=min;
+this.max=max;
+this.sum=sum;
+        }
+    }
+    int ans=0;
+    w fn(TreeNode root){
+if(root==null) return new w(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+w l= fn(root.left);
+w r= fn(root.right);
+
+if(root.val>l.max && root.val<r.min){
+    ans= Math.max(ans,root.val+l.sum+r.sum);
+return new w(Math.min(l.min,root.val),Math.max(r.max,root.val),root.val+l.sum+r.sum);
+}
+return new w(Integer.MIN_VALUE,Integer.MAX_VALUE,0);
+    }
+    public int maxSumBST(TreeNode root) {
+      fn(root);
+        return ans>0?ans:0;
+    }
+}
