861782d5400acb507f9b842377d56495ffe6737b
Upander Garg <upandergarg@gmail.com>
2026-09-15 00:17:18 +0530

    Time: 2 ms (89.25%), Space: 48.1 MB (23.44%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int dis=0;
+        for(int i=0; i<nums.length; i++){
+      
+            if(i>dis) return false;
+                  dis=Math.max(dis,i+nums[i]);
+        }
+        return true;
+    }
+}
