2411eced52ce223f62cf24277c1f1c742a95b234
Upander Garg <upandergarg@gmail.com>
2026-09-18 23:35:59 +0530

    Time: 0 ms (100%), Space: 44.9 MB (58.6%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -14,18 +14,22 @@ class Solution {
     //     return dp[ind]=Math.min(c1,c);
     // }
     public int minCostClimbingStairs(int[] cost) {
-        int dp[]= new int[cost.length+1];
+        // int dp[]= new int[cost.length+1];
         // Arrays.fill(dp,-1);
         // int [] a= Arrays.copyOfRange(cost,1,cost.length);
         // return Math.min(help(cost,cost.length-1,dp),help(cost,a.length-1,dp));
-        dp[0]=cost[0];
+        int prv=cost[0];
+        int cur=0;
+        int prv2=0;
     
         for(int ind=1; ind<cost.length; ind++){
             int c1=cost[ind];
-            if(ind>1) c1+=dp[ind-2];
-int c2= cost[ind]+dp[ind-1];
-dp[ind]=Math.min(c1,c2);
+            if(ind>1) c1+=prv2;
+int c2= cost[ind]+prv;
+cur=Math.min(c1,c2);
+prv2=prv;
+prv=cur;
         }
-        return Math.min(dp[cost.length-1],dp[cost.length-2]);
+        return Math.min(prv,prv2);
     }
 }
